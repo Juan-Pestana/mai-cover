9ce39927d3c114abc12606a017b660d851f13980
Juan Pestana <juan.pestana82@gmail.com>
2023-08-20 03:26:09 +0200

    new url

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,5 +1,5 @@
 export const nextUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  ? `https://mai-cover.com`
   : 'http://localhost:3000'
 
 //`https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || 'http://localhost:3000'
