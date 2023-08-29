cea52afcb58f92be889f1f7f0388dfb76d2364f0
Juan Pestana <juan.pestana82@gmail.com>
2023-08-29 22:52:52 +0200

    test next-auth env2

--- src/app/feedback_form/page.tsx
+++ src/app/feedback_form/page.tsx
@@ -3,6 +3,7 @@ import { montserrat } from '../fonts/fonts'
 import FeedbackForm from '@/components/forms/FeedbackForm'
 
 function Feedback_form_page() {
+  console.log('caca')
   return (
     <main className="flex-1 flex items-center justify-center p-3  ">
       <div className="flex flex-col w-full justify-center items-center ">
